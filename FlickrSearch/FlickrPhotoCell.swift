8a45506b146aaa9c667f30558bb035822e660447
DESKTOP-4V324Q3\daukh <daukhacbac@gmail.com>
2017-05-05 22:08:30 +0700

    no message

---
+++ FlickrSearch/FlickrPhotoCell.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by cntt17 on 5/5/17.
+//  Copyright © 2017 cntt17. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    //
+    @IBOutlet weak var imageView: UIImageView!
+    
+    //
+    
+}
